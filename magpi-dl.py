edcf1e9ce4d4c399730ca73c54c39327a961ba10
matty0005 <matty0005@gmail.com>
2017-01-08 18:58:47 +1000

    Update magpi-dl.py

--- magpi-dl.py
+++ magpi-dl.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import os
 from tqdm import tqdm
 
 url = "https://www.raspberrypi.org/magpi-issues/"
@@ -12,11 +13,18 @@ soup = BeautifulSoup(html.text,"html.parser")
 
 links = tqdm(soup.find_all('a'))
 
+
 for link in links:
     pdf_name = link['href']
     if pdf_name.endswith('.pdf'):
-        with open(pdf_name,'wb') as f:
-            r = requests.get(url+pdf_name,stream=True)
-            for chunk in tqdm(r.iter_content(1024),desc="Downloading " + pdf_name):
-                f.write(chunk)
-            tqdm.write("Downloaded " + pdf_name)
+		downloaded = open('Downloaded.txt', 'r').read()
+		if pdf_name not in downloaded:
+			with open(pdf_name,'wb') as f:
+				r = requests.get(url+pdf_name,stream=True)
+				for chunk in tqdm(r.iter_content(1024),desc="Downloading " + pdf_name):
+					f.write(chunk)
+				tqdm.write("Downloaded " + pdf_name)
+				downloaded = open("Downloaded.txt", "a")
+				downloaded.write(pdf_name + '\n')
+				
+
